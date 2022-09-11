174591b91aa89860aca32090aa79563b9cd7bc3b
Varsha <varsha.pandhare@360degreecloud.in>
2022-09-11 17:27:13 +0530

    Hii I have push then Clear cache test Cases on git

---
+++ Feature/cachedata.feature
@@ -0,0 +1,12 @@
+# Created by Vars-ha at 9/7/2022
+Feature: check Clear cache is Visible
+
+  Scenario: check Clear Cache Option Visible after click on Setting Icon
+    Given enter valid Email & Password
+    When Click on Setting Icon
+    Then check Clear Cache Option visible
+    And close the browser
+  Scenario: Check after clear cache Error msg show
+    Given valid Email & Password
+    When click on clear cache
+    Then check error
---
+++ Feature/steps/cachedata.py
@@ -0,0 +1,49 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+@Given('enter valid Email & Password')
+def login_nopcommerce(self):
+    self.driver = webdriver.Chrome(executable_path="C:\Program Files\Python310\ChromeWebdriver\chromedriver.exe")
+    self.driver.maximize_window()
+    self.driver.get("https://admin-demo.nopcommerce.com/login")
+    self.driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+
+
+@When('Click on Setting Icon')
+def setting_icon(self):
+    self.driver.find_element(By.XPATH, '//a[@data-toggle="dropdown"][1]').click()
+    self.driver.implicitly_wait(10)
+
+
+@Then('check Clear Cache Option visible')
+def visibility(self):
+    elm = self.driver.find_element(By.XPATH, '//span[normalize-space()="Clear cache"]')
+    print(elm.is_displayed())
+
+
+@step("close the browser")
+def closed(self):
+    self.driver.close()
+
+
+@Given('valid Email & Password')
+def clear_cache(self):
+    self.driver = webdriver.Chrome(executable_path="C:\Program Files\Python310\ChromeWebdriver\chromedriver.exe")
+    self.driver.maximize_window()
+    self.driver.get("https://admin-demo.nopcommerce.com/login")
+    self.driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+    self.driver.find_element(By.XPATH, '//a[@data-toggle="dropdown"][1]').click()
+
+
+@When('click on clear cache')
+def click_clear(self):
+    self.driver.find_element(By.XPATH, '//span[normalize-space()="Clear cache"]').click()
+
+
+@Then('check error')
+def error(self):
+    element1 = self.driver.find_element(By.XPATH, '//div[@class="alert alert-danger alert-dismissable"]')
+    print(element1.text)
